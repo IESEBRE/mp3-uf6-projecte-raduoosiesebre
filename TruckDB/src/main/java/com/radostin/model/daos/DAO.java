7029a841baab89236f661e22ac6a67befd10c665
Radostin Ivanov <rivanov@iesebre.com>
2024-05-15 12:26:59 +0200

    in progress

--- TruckDB/src/main/java/com/radostin/model/daos/DAO.java
+++ TruckDB/src/main/java/com/radostin/model/daos/DAO.java
@@ -1,5 +1,15 @@
 package com.radostin.model.daos;
 
-public interface DAO {
+import com.radostin.model.exceptions.DAOException;
 
+import java.util.List;
+
+public interface DAO<T> {
+    T get(Long id) throws DAOException;
+
+    List<T> getAll() throws DAOException;
+
+    void save(T obj) throws DAOException;
+
+    //Tots els mètodes necessaris per interactuar en la BD
 }
